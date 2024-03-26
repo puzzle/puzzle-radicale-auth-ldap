16e723ed8cc4259e4ae007a8c2a87cd1ec4578ec
yoan mueller <ymueller@puzzle.ch>
2024-03-26 13:59:41 +0100

    versioning

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 from distutils.core import setup
 
 setup(
-    name="puzzle-radicale-auth-ldap",
-    version="0.1",
+    name="puzzle_radicale_auth_ldap",
+    version="0.0.2",
     description="LDAP Authentication Plugin for Radicale 3",
     author="Raoul Thill",
     license="GNU GPL v3",
     install_requires=["radicale >= 3.0", "ldap3 >= 2.3"],
     url="https://github.com/puzzle/puzzle-radicale-auth-ldap",
-    download_url="https://github.com/puzzle/puzzle-radicale-auth-ldap/archive/refs/tags/v.0.0.1.tar.gz",
-    packages=["puzzle_radicale_auth_ldap"])
+    download_url="https://github.com/puzzle/puzzle-radicale-auth-ldap/archive/refs/tags/v.0.0.2.tar.gz",
+    packages=["puzzle_radicale_auth_ldap"],
+    python_requires=">=3.6"
+    )
